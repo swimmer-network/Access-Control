ab5f4227b06930e3a000c7a27c92416427ead6fc
lucabada <luan.do1@tiki.vn>
2022-01-25 13:52:02 +0700

    remove initialized value

--- contracts/AccessControlStorage.sol
+++ contracts/AccessControlStorage.sol
@@ -1,9 +1,9 @@
 pragma solidity 0.8.10;
 
 contract SwimmerNetworkACStorage {
-    bytes32 public CREATE_CONTRACT_ROLE = keccak256("CREATE_CONTRACT");
-    bytes32 public VALIDATOR_ROLE = keccak256("VALIDATOR");
-    bytes32 public SETBLACKLIST_ROLE = keccak256("ADD_REMOVE_FROM_TO_LIST");
+    bytes32 public CREATE_CONTRACT_ROLE;
+    bytes32 public VALIDATOR_ROLE ;
+    bytes32 public SETBLACKLIST_ROLE;
 
     mapping (address => uint256) public blockedTime;
 

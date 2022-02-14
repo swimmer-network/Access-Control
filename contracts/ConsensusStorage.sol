749487192233808f341302ac2c81abeab6ed5ab3
lucabada <luan.do1@tiki.vn>
2022-02-14 09:52:37 +0700

    change variable order

--- contracts/ConsensusStorage.sol
+++ contracts/ConsensusStorage.sol
@@ -4,5 +4,4 @@ pragma solidity 0.8.10;
 contract ConsensusStorage {
     uint256 public blockLimit;
     uint256 public gasPrice;
-
 }
--- contracts/RewardPoolStorage.sol
+++ contracts/RewardPoolStorage.sol
@@ -1,9 +1,9 @@
 pragma solidity 0.8.10;
 
 contract RewardPoolStorage {
+    mapping(address => uint256) balanceOfWork;
     address public rewardToken;
     address public accessControlAddress;
-    mapping(address => uint256) balanceOfWork;
     uint256 public lastClaimedTime;
     uint256 public claimedPeriod;
 }

aafea1cc5af7405c7751a3ebeb57a17a9a8e3246
Tung Hoang <hoangtung.cse@gmail.com>
2022-02-25 16:57:29 +0700

    Add TUS contract

---
+++ contracts/TUS.sol
@@ -0,0 +1,54 @@
+pragma solidity 0.8.10;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+
+contract TUS is Ownable, Initializable, ERC20, Pausable {
+    mapping(address=>bool) public minters;
+    modifier onlyMinter(address addr) {
+        require(minters[addr], "TUS: Only minter");
+        _;
+    }
+
+    event SetMinter(address indexed addr, bool status);
+
+    constructor () ERC20("Treasure Under Sea", "TUS") {
+        minters[msg.sender] = true;
+    }
+
+    function initialize(address _owner) external initializer() {
+        minters[_owner] = true;
+        _transferOwnership(_owner);
+    }
+
+    function mint(address _to, uint256 _amount) onlyMinter(msg.sender) external {
+        _mint(_to, _amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    function setMinter(address addr, bool status) onlyOwner() external {
+        minters[addr] = status;
+        emit SetMinter(addr, status);
+    }
+
+    function pause() onlyOwner() external {
+        _pause();
+    }
+
+    function unpause() onlyOwner() external {
+        _unpause();
+    }
+    
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) whenNotPaused() internal override {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
